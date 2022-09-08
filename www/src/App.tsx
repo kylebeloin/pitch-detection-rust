c26038f9f240e69bf30911bef8eaae4b727d13b6
kbeloin <kyle.beloin@me.com>
2022-09-07 22:20:52 -0700

    Added Typescript React without CRM

---
+++ www/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Welcome App thats build using Webpack and Babel separately</h1>
+    </div>
+  );
+};
+
+export default App;
